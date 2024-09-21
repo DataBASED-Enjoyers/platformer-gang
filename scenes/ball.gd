b02ab073f1599ad519c871728ad8d687780cb0e0
Graf von Tee <https://github.com/GrafVonTee>
2024-09-21 21:51:35 +0300

    "Fix" bug

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -2,6 +2,6 @@ class_name Ball
 extends RigidBody2D
 
 
-func get_impluse_from(impulse_source: Vector2, impulse_strength):
+func get_impulse_from(impulse_source: Vector2, impulse_strength):
 	var impulse_direction: Vector2 = (position - impulse_source).normalized()
 	apply_central_impulse(impulse_direction * impulse_strength)
--- scenes/player.gd
+++ scenes/player.gd
@@ -4,24 +4,24 @@ extends CharacterBody2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var ground_detector: Area2D = $Sprite2D/GroundDetector
+@onready var ceiling_detector: Area2D = $Sprite2D/CeilingDetector
 
 @export var speed = 130.0
 @export var jump_velocity = -300.0
 @export var custom_gravity = 1560
-@export var attack_strength = 500
+@export var attack_strength = 100
 
 const PUSH_FORCE = 40.0
-var is_on_ground: bool = false
+
 
 # Functions
 func _physics_process(delta: float) -> void:
-	if not is_on_ground:
+	if not is_on_floor():
 		velocity.y += custom_gravity * delta
 	
-	if Input.is_action_just_pressed("jump") and is_on_ground:
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
-		
+	
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction != 0:
 		sprite_2d.scale.x = direction
@@ -29,16 +29,15 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 	
-	# key function
+	# Key function
 	move_and_slide()
 	
 	# Resolve Collisions with RigidBodies
 	for i in get_slide_collision_count():
 		var c = get_slide_collision(i)
+		print(c.get_collider())
 		if c.get_collider() is RigidBody2D:
 			c.get_collider().apply_central_impulse(-c.get_normal() * PUSH_FORCE)
-			if c.get_position().y < (position.y - sprite_2d.get_rect().size.y * scale.y):
-				c.get_collider().apply_central_impulse(-c.get_normal() * jump_velocity)
 
 
 func _input(event: InputEvent) -> void:
@@ -48,13 +47,4 @@ func _input(event: InputEvent) -> void:
 
 func _on_attack_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("movable_objects"):
-		body.get_impluse_from(position, attack_strength)
-
-
-func _on_ground_detector_body_entered(body: Node2D) -> void:
-	is_on_ground = true
-
-
-func _on_ground_detector_body_exited(body: Node2D) -> void:
-	if ground_detector.get_overlapping_bodies().size() == 0:
-		is_on_ground = false
+		body.get_impulse_from(position, attack_strength)
