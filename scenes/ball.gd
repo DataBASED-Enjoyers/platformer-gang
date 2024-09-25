be8a94b65b5e1bc58c374a9484e1298a3d55d9ec
Graf von Tee <https://github.com/GrafVonTee>
2024-09-26 00:21:14 +0300

    draw for ball with CharacterBody2D class

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -1,7 +1,24 @@
 class_name Ball
-extends RigidBody2D
+extends CharacterBody2D
+
+var has_bounced: bool = true
+@export var gravity = 980
 
 
 func get_impulse_from(impulse_source: Vector2, impulse_strength):
 	var impulse_direction: Vector2 = (position - impulse_source).normalized()
-	apply_central_impulse(impulse_direction * impulse_strength)
+	has_bounced = false
+	velocity = impulse_direction * impulse_strength
+
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+	move_and_slide()
+	apply_bounce()
+
+
+func apply_bounce():
+	if get_slide_collision_count() > 0 and not has_bounced:
+		var collider = get_slide_collision(0)
+		velocity = velocity.bounce(collider.get_normal())
+		has_bounced = true
